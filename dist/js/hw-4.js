7d32d5f62c612e08744791578976db262f2b3574
Pavel_St-skypro <web.zaporozhtsev@gmail.com>
2023-06-16 00:57:41 +0300

    ready for my check in next time

--- dist/js/hw-4.js
+++ dist/js/hw-4.js
@@ -1,2 +1,59 @@
 "use strict";
-let prompt = require("prompt-sync")();
+// let prompt = require("prompt-sync")();
+// let i, inputHello;
+// inputHello = prompt("Enter Hello!: ", "Hello!");
+// i = 1;
+// while (i <= 2) {
+//   ++i;
+//   console.log(inputHello);
+// }
+//!______________________________________________FIRST-1
+
+// let number = 1;
+// for (let index = 1; index <= 5; index++) {
+//   console.log(number++);
+// }
+
+//!______________________________________________SECOND-2
+
+// let number = 7;
+// while (number <= 23) {
+//   console.log(number++);
+// }
+
+//!______________________________________________THIRD-3
+
+// const obj = {
+//   kolya: "200",
+//   vasya: "300",
+//   petya: "200",
+// };
+
+// for (let element in obj) {
+//   console.log(
+//     `${element.charAt(0).toUpperCase() + element.slice(1)} - salary is ${
+//       obj[element]
+//     } dollars`
+//   );
+// }
+
+//!______________________________________________FOURTH-4
+
+// let n = 1000;
+// let num;
+// for (let index = 0; n >= 50; index++) {
+//   n /= 2;
+// }
+// num = n;
+// console.log(num);
+
+//!______________________________________________FIFTH-5
+
+// let friday = 5;
+// let month = 31;
+// for (let i = 1; friday < 31; i++) {
+//   console.log(
+//     `Today is friday, ${friday}nd of month. \nNeed to prepare a report.\n`
+//   );
+//   friday += 7;
+// }
